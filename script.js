76b504f55db2300fde5d5429ac8b50a68391cd66
4lex42 <a.laroy@students.ephec.be>
2025-05-08 09:40:17 +0200

    Initial commit of portfolio

---
+++ script.js
@@ -0,0 +1,18 @@
+
+fetch('projects.json')
+  .then(response => response.json())
+  .then(data => {
+    const container = document.getElementById('projectGallery');
+    data.forEach(proj => {
+      const div = document.createElement('div');
+      div.className = 'project';
+      div.innerHTML = `
+        <img src="\${proj.image}" alt="\${proj.title}">
+        <h3>\${proj.title}</h3>
+        <p>\${proj.description}</p>
+        <a href="\${proj.code}" target="_blank">Code</a> |
+        <a href="\${proj.demo}" target="_blank">Demo</a>
+      `;
+      container.appendChild(div);
+    });
+  });
